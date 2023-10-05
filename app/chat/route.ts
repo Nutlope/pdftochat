93d502b05410d93fe86fec1b5de40be9d5b306a6
nutlope <hassan4709@gmail.com>
2023-10-04 22:03:40 -0400

    still not working

--- app/chat/route.ts
+++ app/chat/route.ts
@@ -10,10 +10,9 @@ import {
   StringOutputParser,
 } from 'langchain/schema/output_parser';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { CONDENSE_TEMPLATE, QA_TEMPLATE } from '@/utils/config';
 import { pinecone } from '@/utils/pinecone-client';
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 
 const combineDocumentsFn = (docs: Document[], separator = '\n\n') => {
   const serializedDocs = docs.map((doc) => doc.pageContent);
@@ -33,11 +32,33 @@ const formatVercelMessages = (chatHistory: VercelChatMessage[]) => {
   return formattedDialogueTurns.join('\n');
 };
 
-const CONDENSE_QUESTION_TEMPLATE = CONDENSE_TEMPLATE;
+const CONDENSE_QUESTION_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+<chat_history>
+{chat_history}
+</chat_history>
+Follow Up Input: {question}
+Standalone question:`;
+
 const condenseQuestionPrompt = PromptTemplate.fromTemplate(
   CONDENSE_QUESTION_TEMPLATE,
 );
-const ANSWER_TEMPLATE = QA_TEMPLATE;
+const ANSWER_TEMPLATE = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+
+<context>
+{context}
+</context>
+
+<chat_history>
+  {chat_history}
+</chat_history>
+
+Question: {question}
+Please return an answer in markdown with clear headings and lists:`;
+
 const answerPrompt = PromptTemplate.fromTemplate(ANSWER_TEMPLATE);
 
 /**
@@ -65,8 +86,8 @@ export async function POST(req: NextRequest) {
       new OpenAIEmbeddings(),
       {
         pineconeIndex: index,
-        textKey: 'text',
         namespace: chatId,
+        textKey: 'text',
       },
     );
 
