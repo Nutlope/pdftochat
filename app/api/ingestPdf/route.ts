4a8c461dd27208d7b4593254bb556ea5934ff501
ogzhanolguncu <ogzhan11@gmail.com>
2024-08-21 15:34:21 +0300

    feat: convert ingest api to rag chat sdk

--- app/api/ingestPdf/route.ts
+++ app/api/ingestPdf/route.ts
@@ -1,35 +1,34 @@
-import { NextResponse } from 'next/server';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import prisma from '@/utils/prisma';
 import { getAuth } from '@clerk/nextjs/server';
-import { loadEmbeddingsModel } from '../utils/embeddings';
-import { loadVectorStore } from '../utils/vector_store';
-import { type MongoClient } from 'mongodb';
+import { RAGChat, togetherai } from '@upstash/rag-chat';
+import { Index } from '@upstash/vector';
+import { NextResponse } from 'next/server';
+
+const ragchat = new RAGChat({
+  vector: new Index(),
+  model: togetherai('togethercomputer/m2-bert-80M-8k-retrieval', {
+    apiKey: process.env.TOGETHER_AI_API_KEY,
+  }),
+  debug: process.env.NODE_ENV == 'development',
+});
 
 export async function POST(request: Request) {
-  let mongoDbClient: MongoClient | null = null;
-
-  const { fileUrl, fileName, vectorStoreId } = await request.json();
-
+  const { fileUrl, fileName } = await request.json();
   const { userId } = getAuth(request as any);
-
+  console.log({ fileUrl, fileName });
   if (!userId) {
     return NextResponse.json({ error: 'You must be logged in to ingest data' });
   }
-
   const docAmount = await prisma.document.count({
     where: {
       userId,
     },
   });
-
   if (docAmount > 3) {
     return NextResponse.json({
       error: 'You have reached the maximum number of documents',
     });
   }
-
   const doc = await prisma.document.create({
     data: {
       fileName,
@@ -37,52 +36,26 @@ export async function POST(request: Request) {
       userId,
     },
   });
-
   const namespace = doc.id;
-
   try {
     /* load from remote pdf URL */
     const response = await fetch(fileUrl);
     const buffer = await response.blob();
-    const loader = new PDFLoader(buffer);
-    const rawDocs = await loader.load();
 
-    /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
+    await ragchat.context.add({
+      fileSource: buffer,
+      type: 'pdf',
+      config: {
+        chunkSize: 1000,
+        chunkOverlap: 200,
+      },
+      options: { namespace },
     });
-    const splitDocs = await textSplitter.splitDocuments(rawDocs);
-    // Necessary for Mongo - we'll query on this later.
-    for (const splitDoc of splitDocs) {
-      splitDoc.metadata.docstore_document_id = namespace;
-    }
-
     console.log('creating vector store...');
-
-    /* create and store the embeddings in the vectorStore */
-    const embeddings = loadEmbeddingsModel();
-
-    const store = await loadVectorStore({
-      namespace: doc.id,
-      embeddings,
-    });
-    const vectorstore = store.vectorstore;
-    if ('mongoDbClient' in store) {
-      mongoDbClient = store.mongoDbClient;
-    }
-
-    // embed the PDF documents
-    await vectorstore.addDocuments(splitDocs);
   } catch (error) {
     console.log('error', error);
     return NextResponse.json({ error: 'Failed to ingest your data' });
-  } finally {
-    if (mongoDbClient) {
-      await mongoDbClient.close();
-    }
   }
-
   return NextResponse.json({
     text: 'Successfully embedded pdf',
     id: namespace,
